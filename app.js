9af4128671e214bd1d0a37be52dd00c76e8ce80e
Iman <iman.mansour94@gmail.com>
2018-05-13 22:45:09 +0200

    lissten for slack messages

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var slackEventAdapter = require('@slack/events-api').createSlackEventAdapter;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+var slackEvents = slackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +22,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// make the event handler route
+app.use('/slack/events', [slackEvents.expressMiddleware(), indexRouter]);
+
+// attach listener to message event
+slackEvents.on('message', (message)=> {
+
+  if(message.text.indexOf('go') != -1){ // if 'go' messege is typed ==> go and fetch tweets
+
+  // fetch tweets here
+
+  }
+});
+
+// Handle errors (see `errorCodes` export)
+slackEvents.on('error', console.error);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
